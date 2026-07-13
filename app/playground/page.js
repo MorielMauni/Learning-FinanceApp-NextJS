8172f9a6931d98710b301118b17d28e0ddf43c23
Moriel Mauni <morielmauni@gmail.com>
2026-07-13 20:33:39 +0000

    Add: Transaction summary + prettier text

--- app/playground/page.js
+++ app/playground/page.js
@@ -1,6 +1,7 @@
 import PageHeader from "@/components/header";
 import TransactionItem from "@/components/transaction-item";
 import Trend from "@/components/trend";
+import TransactionSummaryItem from "@/components/transaction-summary-item";
 
 export default function playground() {
   return (
@@ -11,7 +12,7 @@ export default function playground() {
         <h2 className="mb-4 text-lg font-mono">PageHeader</h2>
         <hr className="mb-4 border-grey-200 dark:border-gray-800"></hr>
         <div>
-          <PageHeader/>
+          <PageHeader />
         </div>
       </div>
 
@@ -19,10 +20,10 @@ export default function playground() {
         <h2 className="mb-4 text-lg font-mono">Trend</h2>
         <hr className="mb-4 border-grey-200 dark:border-gray-800"></hr>
         <div className="flex space-x-8">
-          <Trend type="Income" amount={1000} prevAmount={900}/>
-          <Trend type="Expense" amount={1200} prevAmount={10000}/>
-          <Trend type="Investment" amount={7000} prevAmount={11100}/>
-          <Trend type="Saving" amount={500} prevAmount={950}/>
+          <Trend type="Income" amount={1000} prevAmount={900} />
+          <Trend type="Expense" amount={1200} prevAmount={10000} />
+          <Trend type="Investment" amount={7000} prevAmount={11100} />
+          <Trend type="Saving" amount={500} prevAmount={950} />
         </div>
       </div>
 
@@ -30,13 +31,54 @@ export default function playground() {
         <h2 className="mb-4 text-lg font-mono">Transaction Item</h2>
         <hr className="mb-4 border-grey-200 dark:border-gray-800"></hr>
         <div className="space-y-4">
-          <TransactionItem type='Income' desciption='Salary' amount={2000}/>
-          <TransactionItem type='Expense' category='Food' desciption='Date night' amount={200}/>
-          <TransactionItem type='Saving' category='Saving' desciption='For the kids' amount={300}/>
-          <TransactionItem type='Investment' category='Stocks' desciption='S&P' amount={1000}/>
+          <TransactionItem type="Income" desciption="Salary" amount={2000} />
+          <TransactionItem
+            type="Expense"
+            category="Food"
+            desciption="Date night"
+            amount={200}
+          />
+          <TransactionItem
+            type="Saving"
+            category="Saving"
+            desciption="For the kids"
+            amount={300}
+          />
+          <TransactionItem
+            type="Investment"
+            category="Stocks"
+            desciption="S&P"
+            amount={1000}
+          />
         </div>
       </div>
-    
+
+      <div>
+        <h2 className="mb-4 text-lg font-mono">Transaction summary + Item</h2>
+        <hr className="mb-4 border-grey-200 dark:border-gray-800"></hr>
+        <div className="space-y-4">
+          <TransactionSummaryItem date="2026-12-25" amount={3500} />
+          <TransactionItem type="Income" desciption="Salary" amount={2000} />
+          <TransactionItem
+            type="Expense"
+            category="Food"
+            desciption="Date night"
+            amount={200}
+          />
+          <TransactionItem
+            type="Saving"
+            category="Saving"
+            desciption="For the kids"
+            amount={300}
+          />
+          <TransactionItem
+            type="Investment"
+            category="Stocks"
+            desciption="S&P"
+            amount={1000}
+          />
+        </div>
+      </div>
     </main>
   );
 }
